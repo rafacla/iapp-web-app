6e148354aae2d2e9f2ed7c19a5953ef542aa970c
Rafael <rafacla@users.noreply.github.com>
2019-07-13 06:39:46 -0300

    implementação do controle de data do orçamento

--- src/app/componentes/orcamento/orcamento.component.ts
+++ src/app/componentes/orcamento/orcamento.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { MatDatepicker } from '@angular/material';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-orcamento',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orcamento.component.css']
 })
 export class OrcamentoComponent implements OnInit {
-
+  orcamento_data = moment().startOf('month');
   constructor() { }
 
   ngOnInit() {
+  }
+
+  chosenYearHandler(normalizedYear: moment.Moment) {
+    this.orcamento_data.year(normalizedYear.year());
+  }
+  chosenMonthHandler(normalizedMonth: moment.Moment, datepicker: MatDatepicker<moment.Moment>) {
+    this.orcamento_data.month(normalizedMonth.month());
+    this.orcamento_data = moment(this.orcamento_data);
+    datepicker.close();
+  }
+  subtraiMes() {
+    this.orcamento_data.subtract(1,'month').startOf('month');
+    this.orcamento_data = moment(this.orcamento_data);
+  }
+  adicionaMes() {
+    this.orcamento_data.add(1,'month').startOf('month');
+    this.orcamento_data = moment(this.orcamento_data);
   }
 
 }
