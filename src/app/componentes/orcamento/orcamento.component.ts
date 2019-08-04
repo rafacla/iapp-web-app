3f1188d06b8a041d3ee5fe24c6439724887a2284
Rafael <rafacla@users.noreply.github.com>
2019-08-04 08:52:18 -0300

    implementado função para trocar o mês da view orçamento

--- src/app/componentes/orcamento/orcamento.component.ts
+++ src/app/componentes/orcamento/orcamento.component.ts
@@ -5,6 +5,7 @@ import { FormControl } from '@angular/forms';
 import { HttpClientService } from 'src/app/servicos/comunicacao/http_client.service';
 import { UserService } from 'src/app/servicos/user/user.service';
 import { OrcamentoList, OrcamentoGet } from 'src/app/data-model/orcamento-list';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-orcamento',
@@ -12,7 +13,8 @@ import { OrcamentoList, OrcamentoGet } from 'src/app/data-model/orcamento-list';
   styleUrls: ['./orcamento.component.css']
 })
 export class OrcamentoComponent implements OnInit {
-  orcamento_data = moment().startOf('month');
+  orcamento_data = new BehaviorSubject<moment.Moment>(moment().startOf('month'));
+  orcamento_data$ = this.orcamento_data.asObservable();
   dataSource = new MatTableDataSource<OrcamentoList>([]);
   categoriasColumns: string[] = ['categoria_nome', 'orcamento_valor', 'transacoes_valor'];
   subcategoriasColumns: string[] = ['subcategoria_nome', 'orcamento_valor', 'transacoes_valor'];
@@ -22,12 +24,14 @@ export class OrcamentoComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.orcamentosGet();
+    //this.orcamentosGet();
+    this.orcamento_data$.subscribe(valor => this.orcamentosGet());
   }
 
   orcamentosGet() {
+    this.dataSource.data = [];
     this.userService.getUserDetail().subscribe(userDetail => {
-      this.http.orcamentosGet(userDetail.userLastDiarioUID, this.orcamento_data.format('MM'), this.orcamento_data.format('YYYY')).subscribe(orcamento => {
+      this.http.orcamentosGet(userDetail.userLastDiarioUID, this.orcamento_data.value.format('MM'), this.orcamento_data.value.format('YYYY')).subscribe(orcamento => {
         this.dataSource.data = orcamento.lista_orcamentos;
       });
     });
@@ -36,20 +40,20 @@ export class OrcamentoComponent implements OnInit {
     return row.subcategoria_is;
   }
   chosenYearHandler(normalizedYear: moment.Moment) {
-    this.orcamento_data.year(normalizedYear.year());
+    this.orcamento_data.value.year(normalizedYear.year());
   }
   chosenMonthHandler(normalizedMonth: moment.Moment, datepicker: MatDatepicker<moment.Moment>) {
-    this.orcamento_data.month(normalizedMonth.month());
-    this.orcamento_data = moment(this.orcamento_data);
+    this.orcamento_data.value.month(normalizedMonth.month());
+    this.orcamento_data.next(moment(this.orcamento_data.value));
     datepicker.close();
   }
   subtraiMes() {
-    this.orcamento_data.subtract(1,'month').startOf('month');
-    this.orcamento_data = moment(this.orcamento_data);
+    this.orcamento_data.value.subtract(1,'month').startOf('month');
+    this.orcamento_data.next(moment(this.orcamento_data.value));
   }
   adicionaMes() {
-    this.orcamento_data.add(1,'month').startOf('month');
-    this.orcamento_data = moment(this.orcamento_data);
+    this.orcamento_data.value.add(1,'month').startOf('month');
+    this.orcamento_data.next(moment(this.orcamento_data.value));
   }
 
 }
