cdce1b382bc35f49dc9cc994bea8ba79fa88a437
Rafael Claudio <rafael.claudio@br.abb.com>
2019-09-19 18:25:02 -0300

    tentando ativar https e resolver rotas

--- server.js
+++ server.js
@@ -7,16 +7,13 @@ app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'dist', 'iapp-web-app'
 )))
 
-app.get('*', function(req, res) {  
-   res.redirect('https://' + req.headers.host + req.url);
+app.get('/*', function (req, res) {
+   if (req.secure) {
+      res.sendFile(path.join(__dirname, 'dist', 'iapp-web-app', 'index.html'))
+   } else {
+      res.redirect('https://' + req.headers.host + req.url);
+   }
+   
 })
- 
-app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, 'dist', 'iapp-web-app', 'index.html'))
-})
-
-app.use(function(req, res) {
-   res.redirect('/');
-});
  
 app.listen(port)
