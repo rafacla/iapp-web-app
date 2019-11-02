f6d70326925c0d048d4ec5220d0d97b33bbbfc67
Rafael <rafacla@users.noreply.github.com>
2019-11-02 07:38:39 -0300

    forçar https

--- server.js
+++ server.js
@@ -10,6 +10,22 @@ app.use(
    )
 )
 
+app.get('*', (req, res, next) => {
+   if (req.headers['x-forwarded-proto'] != 'https') {
+       
+// checa se o header é HTTP ou HTTPS
+
+       res.redirect("https://" + req.headers.host + req.url);
+       
+// faz o redirect para HTTPS
+
+   } else {
+       next();
+       
+// segue com a sequência das rotas
+
+   }
+});
 
 app.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, 'dist', 'iapp-web-app', 'index.html'));   
