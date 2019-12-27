d57282a6b18284c90c89456ab5e560ed8d72c117
Rafael <rafacla@users.noreply.github.com>
2019-12-27 09:28:30 -0300

    mais uma tentativa de resolver o redirecionamento para https forçado

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.use(
 )
 
 app.get('*', (req, res, next) => {
+   if (req.headers['x-forwarded-proto'] != 'https') { 
+      // checa se o header é HTTP ou HTTPS
+       res.redirect("https://" + req.headers.host + req.url); 
+      // faz o redirect para HTTPS
+   } else {
+       next();
+      // segue com a sequência das rotas
+   }
+});
+
+app.get('/', (req, res, next) => {
    if (req.headers['x-forwarded-proto'] != 'https') {
        
 // checa se o header é HTTP ou HTTPS
@@ -27,7 +38,7 @@ app.get('*', (req, res, next) => {
    }
 });
 
-app.get('/', (req, res, next) => {
+app.get('/*', (req, res, next) => {
    if (req.headers['x-forwarded-proto'] != 'https') {
        
 // checa se o header é HTTP ou HTTPS
